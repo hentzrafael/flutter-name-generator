2c33095a1f311b5fbb57647ae998e1c12583646d
Rafael Hentz <rafaelhentz172@gmail.com>
2022-07-25 16:09:40 -0300

    Added switch to dark mode on Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -19,31 +19,30 @@ class _MyAppState extends State<MyApp> {
   DarkThemeProvider themeChangeProvider = DarkThemeProvider();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getCurrentAppTheme();
   }
 
   void getCurrentAppTheme() async {
-    themeChangeProvider.darkTheme = await themeChangeProvider.darkThemePreference.getTheme();
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create:(_){
-      return themeChangeProvider;
-    },
-    child: Consumer<DarkThemeProvider>(
-      builder: (context, value, child) {
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
+      },
+      child: Consumer<DarkThemeProvider>(builder: (context, value, child) {
         return MaterialApp(
-        title: 'Name Generator',
-        theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-        debugShowCheckedModeBanner: false,
-        home: const RandomWords());
+            title: 'Name Generator',
+            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+            debugShowCheckedModeBanner: false,
+            home: const RandomWords());
       }),
     );
-
-    
   }
 }
 
@@ -58,7 +57,6 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
-
   void _pushSaved() {
     Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
       final tiles = _saved.map((pair) {
@@ -92,6 +90,17 @@ class _RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: const Text('Name Generator'),
         actions: [
+          Switch(
+              activeThumbImage: const AssetImage(
+                'images/moonColored.png',
+              ),
+              inactiveThumbImage: const AssetImage(
+                'images/sunColored.jpg',
+              ),
+              value: themeChange.darkTheme,
+              onChanged: (bool? value) {
+                themeChange.darkTheme = value!;
+              }),
           IconButton(
             onPressed: _pushSaved,
             icon: const Icon(Icons.list),
@@ -99,51 +108,37 @@ class _RandomWordsState extends State<RandomWords> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              const Text('Dark Mode'),
-              Checkbox(value: themeChange.darkTheme,
-               onChanged: (bool? value){
-                themeChange.darkTheme = value!;
-              }),
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemBuilder: (context, i) {
-              if (i.isOdd) return const Divider();
-                
-              final index = i ~/ 2;
-              if (index >= _suggestions.length) {
-                _suggestions.addAll(generateWordPairs().take(10));
-              }
-              final alreadySaved = _saved.contains(_suggestions[index]);
-              return ListTile(
-                title: Text(
-                  _suggestions[index].asPascalCase,
-                  style: _biggerFont,
-                ),
-                trailing: Icon(
-                  alreadySaved ? Icons.favorite : Icons.favorite_border,
-                  color: alreadySaved ? Colors.red : null,
-                  semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-                ),
-                onTap: () {
-                  setState(() {
-                    if (alreadySaved) {
-                      _saved.remove(_suggestions[index]);
-                    } else {
-                      _saved.add(_suggestions[index]);
-                    }
-                  });
-                },
-              );
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          final alreadySaved = _saved.contains(_suggestions[index]);
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
             },
-                  ),
-          ),]
+          );
+        },
       ),
     );
   }
